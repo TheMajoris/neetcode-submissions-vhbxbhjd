d89124178f0ded153655326605f1906bde14365a
Alex <74721713+TheMajoris@users.noreply.github.com>
2026-08-21 13:48:59 +0800

    Add: is-anagram - submission-25

---
+++ Data Structures & Algorithms/is-anagram/submission-25.py
@@ -0,0 +1,27 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        count = {}
+        idx = 0
+        for i in s:
+            if(i in count):
+                count[i] += 1
+                idx += 1
+            else:
+                count[i] = 1
+                idx += 1
+
+        for j in t:
+            if(j in count):
+                count[j] -= 1
+                idx -= 1
+                if(count[j] < 0):
+                    return False
+            else:
+                return False
+
+        if idx != 0:
+            return False
+    
+        return True
+
+        
